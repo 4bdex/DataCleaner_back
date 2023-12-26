09eaaf6117177b0b2f690ab4e0da42177c3cc754
Abdex <abdex4social@gmail.com>
2023-12-26 11:13:50 +0100

    update

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 from flask import jsonify
-from controllers.dataset_controller import upload_dataset, get_dataset
-from controllers.Textual_controller import replaceString,removeHTML,removeSpecialCharacters,tokenize,lowercase,removeStopwords,stemming,lemmatization,removeDuplicates, spellChecking,cleanWithCustomPatterns,handleEncodingIssues,removeWhitespaces,getTextFromHTML
+from controllers.dataset_controller import upload_dataset, get_dataset,upload_dataset2
+from controllers.Textual_controller import replaceString,removeHTML,removeSpecialCharacters,tokenize,lowercase,removeStopwords,stemming,lemmatization,removeDuplicates, spellChecking,cleanWithCustomPatterns,handleEncodingIssues,removeWhitespaces,getTextFromHTML,wordEmbedding
 
 def init_app_routes(app):
     app.add_url_rule('/upload', 'upload_dataset', upload_dataset, methods=['POST'])
+    app.add_url_rule('/upload2', 'upload_dataset2', upload_dataset2, methods=['POST'])
     app.add_url_rule('/get_dataset/<string:dataset_id>', 'get_dataset', get_dataset, methods=['GET'])
     app.add_url_rule('/replaceString', 'replaceString', replaceString, methods=['POST'])
     app.add_url_rule('/removeHTML', 'removeHTML', removeHTML, methods=['POST'])
@@ -19,6 +20,7 @@ def init_app_routes(app):
     app.add_url_rule('/removeWhitespaces', 'removeWhitespaces', removeWhitespaces, methods=['POST'])
     app.add_url_rule('/getTextFromHTML', 'getTextFromHTML', getTextFromHTML, methods=['POST'])
     app.add_url_rule('/lowercase', 'lowercase', lowercase, methods=['POST'])
+    app.add_url_rule('/wordEmbedding', 'wordEmbedding', wordEmbedding, methods=['POST'])
     #app.add_url_rule('/tfidf', 'tfidf', tfidf, methods=['POST'])
     
     
